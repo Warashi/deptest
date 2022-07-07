323d797b3f53d1d168eb655cd637dd52a982f225
Shinnosuke Sawada <6warashi9@gmail.com>
2022-07-07 09:31:01 +0900

    feat: return coverprofile directory if not exists

--- cmd/deptest/main.go
+++ cmd/deptest/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/Warashi/deptest"
@@ -15,6 +18,10 @@ func main() {
 	filename := flag.Arg(0)
 	profile, err := cover.ParseProfiles(filename)
 	if err != nil {
+		if errors.Is(os.ErrNotExist, err) {
+			fmt.Println(filepath.Dir(filename))
+			return
+		}
 		log.Fatalf("cover.ParseProfiles(%q): %v", filename, err)
 	}
 
